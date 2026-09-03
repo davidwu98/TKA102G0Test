d4c3f430291dcf1659de1a987949e6dca9acf1bb
K.H.Wu <kuw294523@gmail.com>
2026-09-03 15:25:26 +0800

    新增ForumVO完成

---
+++ src/main/java/com/tibame/forum/model/ForumVO.java
@@ -0,0 +1,19 @@
+package com.tibame.forum.model;
+
+public class ForumVO {
+	private Integer forumId;
+	private String title;
+	public Integer getForumId() {
+		return forumId;
+	}
+	public void setForumId(Integer forumId) {
+		this.forumId = forumId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
