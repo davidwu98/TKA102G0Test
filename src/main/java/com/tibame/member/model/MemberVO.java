3376c57cbda3ea6c42d75480d078959f1d550298
David Wu <vladylo98@gmail.com>
2026-09-03 14:51:41 +0800

    First commit

---
+++ src/main/java/com/tibame/member/model/MemberVO.java
@@ -0,0 +1,22 @@
+package com.tibame.member.model;
+
+public class MemberVO {
+	private Integer xxx;
+	private String name;
+	
+	public Integer getXxx() {
+		return xxx;
+	}
+	public void setXxx(Integer xxx) {
+		this.xxx = xxx;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
